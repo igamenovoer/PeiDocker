d919ebf80adbbddfbf3dc072708a75ff3deb9268
igamenovoer <igamenover@gmail.com>
2024-07-02 13:06:16 +0800

    added cuda support

--- test-generate-compose.py
+++ test-generate-compose.py
@@ -4,10 +4,14 @@ from rich import print
 fn_template = r'templates\base-image.yml'
 fn_output = r'docker-compose.yml'
 cfg_source = oc.OmegaConf.load(fn_template)
-# cfg_source['x-config'].apt.source_file=''
+cfg_source['x-run-cfg'].device='gpu'
+# cfg_source['x-build-cfg'].apt.source_file=''
 cfg = cfg_source.copy()
 oc.OmegaConf.resolve(cfg)
-del cfg['x-config']
+del cfg['x-build-cfg']
+del cfg['x-run-cfg']
+del cfg['x-sections']
+
 cfg_yaml = oc.OmegaConf.to_yaml(cfg)
 print(cfg_yaml)
 
