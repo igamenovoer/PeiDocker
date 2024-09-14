8cbf74937cf34301147d90b4969f6d5f6458390e
igamenovoer <igamenovoer@gmail.com>
2024-09-15 02:45:29 +0800

    updated invoke ai startup script, added INVOKEAI_AUTO_START env variable

--- pei_docker/project_files/installation/stage-2/system/invoke-ai/start-invokeai.sh
+++ pei_docker/project_files/installation/stage-2/system/invoke-ai/start-invokeai.sh
@@ -3,13 +3,26 @@
 # run invokeai on startup
 # this script should be copied to stage-2/custom
 
-echo "Starting InvokeAI on startup"
+# if INVOKEAI_AUTO_START does not exist, set it to 1
+if [ -z "$INVOKEAI_AUTO_START" ]; then
+    export INVOKEAI_AUTO_START=1
+fi
 
-# get directory of this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# if INVOKEAI_AUTO_START is set to 0 or false, exit
+if [ $INVOKEAI_AUTO_START -eq 0 ]; then
+    echo "INVOKEAI_AUTO_START is set to 0, exit"
+elif [ $INVOKEAI_AUTO_START == "false" ]; then
+    echo "INVOKEAI_AUTO_START is set to false, exit"
+else
+    echo "INVOKEAI_AUTO_START is set to $INVOKEAI_AUTO_START, starting InvokeAI on startup"
+    # echo "Starting InvokeAI on startup"
 
-# start invoke ai services for multiple users
-target_script="$DIR/../system/invoke-ai/run-invoke-ai-multi-user.sh"
+    # get directory of this script
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# run it
-bash $target_script
+    # start invoke ai services for multiple users
+    target_script="$DIR/../system/invoke-ai/run-invoke-ai-multi-user.sh"
+
+    # run it
+    bash $target_script
+fi
